43b51f16b4ed06d9ab80638095950f106e0b3d0e
askayahia <aska.yahia@gmail.com>
2023-04-27 19:35:25 -0700

    push

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,12 +1,14 @@
-#include<stdio.h>
+#include <stdio.h>
+
+void first(void) __attribute__ ((constructor));
 
 /**
- * before_main - Apply constructor attribute the executed before main()
- * Return: nothing
+ * first - prints a sentence before the main
+ * function is executed
  */
-
-void before_main(void) __attribute__ ((constructor))
+void first(void)
 {
 	printf("You're beat! and yet, you must allow,\n");
 	printf("I bore my house upon my back!\n");
 }
+
--- 0x12-singly_linked_lists/README.md
+++ 0x12-singly_linked_lists/README.md
@@ -1,5 +1,1 @@
-0. Print list
-1. List length
-2. Add node
-3. Add node at the end
-4. Free list
+0x12. C - Singly linked lists
